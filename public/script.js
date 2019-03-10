644a5bd38c8df36ac1757cd8a38e7d500923df6b
Adam LaSpina <alaspina6@gmail.com>
2019-03-10 18:34:50 -0400

    App now clears previous search results

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 var xhr = new XMLHttpRequest();
 
 function search(){
+    var results = document.getElementById("searchResults");
+    deleteAllChildren(results);
+    //alert(results.firstChild);
+
     var term = document.getElementById("searchBox").value;
     xhr.open('GET', "https://images-api.nasa.gov/search?q=" + term + "&media_type=image", true);
     xhr.send();   
     
     xhr.addEventListener("readystatechange", processRequest, false);
+}
+
+//Recursive function to delete all elements in parent element node
+function deleteAllChildren(node){
+    while(node.firstChild != null){
+        deleteAllChildren(node.firstChild);
+        node.removeChild(node.firstChild);
+    }
 }
 
 function processRequest(e) {
@@ -14,12 +26,12 @@ function processRequest(e) {
     if(xhr.readyState == 4 && xhr.status == 200){
         var response = JSON.parse(xhr.responseText);
 
-        alert("here");
+        //alert("here");
         var pic; var i;
         for(i = 0; i < response.collection.metadata.total_hits; i++){
             pic = document.createElement("IMG");
             pic.setAttribute("src", response.collection.items[i].links[0].href);
-            document.body.appendChild(pic);
+            document.getElementById("searchResults").appendChild(pic);
         }
     }
 }
